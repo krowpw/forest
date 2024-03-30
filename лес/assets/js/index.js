cc0ca050ae8e768d6497702f6032992214e983ee
krowpw <130204567+krowpw@users.noreply.github.com>
2024-03-30 18:17:20 +1000

    Add files via upload

--- лес/assets/js/index.js
+++ лес/assets/js/index.js
@@ -1,6 +1,7 @@
-document.addEventListener("mousemove", (e)=>{ 
-    Object.assign(document.documentElement,{
-        style: `--moveX: ${e.clientX - window.innerHeight}deg; --moveY: ${e.clientX - window.innerWidth}deg`,
-    
-    });
+document.addEventListener("mousemove", (e) => {
+  Object.assign(document.documentElement, {
+    style: `--moveX: ${
+      (e.clientX - window.innerWidth / 2) * -0.005
+    }deg; --moveY: ${(e.clientY - window.innerHeight / 2) * 0.01}deg`,
+  });
 });
